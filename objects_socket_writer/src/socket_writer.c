4431817df5ac41306ca7b511c23ee75bc43953b2
WeigangZhu <Zhu_Dean@outlook.com>
2017-11-26 15:56:30 +0800

    modified some functions and move some functions to other file

--- objects_socket_writer/src/socket_writer.c
+++ objects_socket_writer/src/socket_writer.c
@@ -1,201 +1,6 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <signal.h>
-#include <unistd.h>
-
-// API headers
-#include "polysync_core.h"
-#include "polysync_node.h"
-#include "polysync_sdf.h"
-#include "polysync_message.h"
-#include "polysync_socket.h"
-#include "polysync_node_template.h"
-
-
-
-
-// *****************************************************
-// static global types/macros
-// *****************************************************
-
-/**
- * @brief Node flags to be OR'd with driver/interface flags.
- *
- * Provided by the compiler so Harbrick can add build-specifics as needed.
- *
- */
-#ifndef NODE_FLAGS_VALUE
-#define NODE_FLAGS_VALUE (0)
-#endif
-
-
-/**
- * @brief Port number this example connects to.
- *
- */
-#define UDP_PORT (9966)
-
-
-/**
- * @brief IP address this example connects to.
- *
- */
-static const char UDP_ADDRESS[] = "192.168.1.201";
-
-
-/**
- * @brief PolySync node name.
- *
- */
-static const char NODE_NAME[] = "polysync-socket-writer-c";
-
-
-/**
- * @brief objects message type name.
- *
- */
-static const char OBJECTS_MSG_NAME[] = "ps_objects_msg";
-
+#include"ps_func.h"
 //
 ps_socket *my_socket = NULL;
-
-
-// *****************************************************
-// static declarations
-// *****************************************************
-
-/**
- * @brief Message "ps_diagnostic_trace_msg" handler.
- *
- * Prints the diagnostic information information of the node who published the message.
- *
- * @param [in] msg_type Message type identifier for the message, as seen by the data model.
- * @param [in] message Message reference to be handled by the function.
- * @param [in] user_data A pointer to void which specifies the internal data.
- *
- */
-static void ps_objects_msg__handler(
-        const ps_msg_type msg_type,
-        const ps_msg_ref const message,
-        void * const user_data );
-
-
-/**
- * @brief Node template set configuration callback function.
- *
- * If the host provides command line arguments they will be set, and available
- * for parsing (ie getopts).
- *
- * @note Returning a DTC other than DTC_NONE will cause the node to transition
- * into the fatal state and terminate.
- *
- * @param [in] node_config A pointer to \ref ps_node_configuration_data which specifies the configuration.
- *
- * @return DTC code:
- * \li \ref DTC_NONE (zero) if success.
- *
- */
-static int set_configuration(
-        ps_node_configuration_data * const node_config );
-
-
-/**
- * @brief Node template on_init callback function.
- *
- * Called once after node transitions into the INIT state.
- *
- * @param [in] node_ref Node reference, provided by node template API.
- * @param [in] state A pointer to \ref ps_diagnostic_state which stores the current state of the node.
- * @param [in] user_data A pointer to user data, provided by user during configuration.
- *
- */
-static void on_init(
-        ps_node_ref const node_ref,
-        const ps_diagnostic_state * const state,
-        void * const user_data );
-
-
-/**
- * @brief Node template on_release callback function.
- *
- * Called once on node exit.
- *
- * @param [in] node_ref Node reference, provided by node template API.
- * @param [in] state A pointer to \ref ps_diagnostic_state which stores the current state of the node.
- * @param [in] user_data A pointer to user data, provided by user during configuration.
- *
- */
-static void on_release(
-        ps_node_ref const node_ref,
-        const ps_diagnostic_state * const state,
-        void * const user_data );
-
-
-/**
- * @brief Node template on_error callback function.
- *
- * Called continously while in ERROR state.
- *
- * @param [in] node_ref Node reference, provided by node template API.
- * @param [in] state A pointer to \ref ps_diagnostic_state which stores the current state of the node.
- * @param [in] user_data A pointer to user data, provided by user during configuration.
- *
- */
-static void on_error(
-        ps_node_ref const node_ref,
-        const ps_diagnostic_state * const state,
-        void * const user_data );
-
-
-/**
- * @brief Node template on_fatal callback function.
- *
- * Called once after node transitions into the FATAL state before terminating.
- *
- * @param [in] node_ref Node reference, provided by node template API.
- * @param [in] state A pointer to \ref ps_diagnostic_state which stores the current state of the node.
- * @param [in] user_data A pointer to user data, provided by user during configuration.
- *
- */
-static void on_fatal(
-        ps_node_ref const node_ref,
-        const ps_diagnostic_state * const state,
-        void * const user_data );
-
-
-/**
- * @brief Node template on_warn callback function.
- *
- * Called continously while in WARN state.
- *
- * @param [in] node_ref Node reference, provided by node template API.
- * @param [in] state A pointer to \ref ps_diagnostic_state which stores the current state of the node.
- * @param [in] user_data A pointer to user data, provided by user during configuration.
- *
- */
-static void on_warn(
-        ps_node_ref const node_ref,
-        const ps_diagnostic_state * const state,
-        void * const user_data );
-
-
-/**
- * @brief Node template on_ok callback function.
- *
- * Called continously while in OK state.
- *
- * @param [in] node_ref Node reference, provided by node template API.
- * @param [in] state A pointer to \ref ps_diagnostic_state which stores the current state of the node.
- * @param [in] user_data A pointer to user data, provided by user during configuration.
- *
- */
-static void on_ok(
-        ps_node_ref const node_ref,
-        const ps_diagnostic_state * const state,
-        void * const user_data );
-
-
-
 
 // *****************************************************
 // static definitions
@@ -209,36 +14,21 @@ static void ps_objects_msg__handler(
     
     // local vars
     int ret = DTC_NONE;
-    //const ps_objects_msg * const objects_msg = (ps_objects_msg*) message;
     char buffer[1248];
     unsigned long buffer_size = 0;
     unsigned long bytes_written = 0;
     ps_socket *socket = NULL;
 
-
     // cast
     socket = (ps_socket*) my_socket;
-
-    // check reference since other routines don't
-    if( socket == NULL )
-    {
-        psync_log_message(
-                LOG_LEVEL_ERROR,
-                "%s : (%u) -- invalid socket",
-                __FILE__,
-                __LINE__ );
-
-        //psync_node_activate_fault( node_ref, DTC_USAGE, NODE_STATE_FATAL );
-        return;
-    }
-
+	ps_socket_error(socket);
+    
     // zero
     memset( buffer, 0, sizeof(buffer) );
-
+    
     // copy bytes into buffer
     strcpy( buffer, "message from socket write");
-
-
+    
     // set buffer size
     buffer_size = strlen(buffer) + 1;
 
@@ -252,55 +42,45 @@ static void ps_objects_msg__handler(
             (unsigned char*) buffer,
             buffer_size,
             &bytes_written );
+	ps_socket_send_error(ret);
 
-    // activate fatal error and return if failed
-    if( ret != DTC_NONE )
-    {
-        psync_log_message(
-                LOG_LEVEL_ERROR,
-                "%s : (%u) -- psync_socket_send_to returned DTC %d",
-                __FILE__,
-                __LINE__,
-                ret );
-
-       // psync_node_activate_fault( node_ref, ret, NODE_STATE_FATAL );
-        return;
-    }
+    ps_printf(message);
+}
 
 
-    // cast to message
-    const ps_objects_msg * const objects_msg = (ps_objects_msg*) message;
-
-    // print details
-    printf( "  received objects message from node 0x%016llX (%llu)\n",
-            (unsigned long long) objects_msg->header.src_guid,
-            (unsigned long long) objects_msg->header.src_guid );
-
-    printf( "  received objects message from node 0x%016llX (%llu)\n",
-            (unsigned long long) objects_msg->header.timestamp,
-            (unsigned long long) objects_msg->header.timestamp );
-
-    unsigned long objects_index = 0;
-    
-    while( objects_index < objects_msg->objects._length )
-    {
-
-        const ps_object *_buffer = objects_msg->objects._buffer;
+/*------------------------------------------------------------------------------------------------------------------------*/
+/*------------------------------------------------------------------------------------------------------------------------*/
+/*------------------------------------------------------------------------------------------------------------------------*/
+/*------------------------------------------------------------------------------------------------------------------------*/
 
 
-        printf( "  - publishers objects :\n");
-        printf( "  received identifier id = 0x%016llX\n",
-                (unsigned long long) _buffer[objects_index].id);
-		printf( "  received position:\nx = %016lf\ny = %016lf\nz = %016lf\n",
-                (double) _buffer[objects_index].position[0],
-		(double) _buffer[objects_index].position[1],
-		(double) _buffer[objects_index].position[2]);
 
-        objects_index++;
-        
-    }
+// *****************************************************
+// public definitions
+// *****************************************************
 
-    printf( "\n" );
+//
+int main( int argc, char **argv )
+{
+    // callback data
+    ps_node_callbacks callbacks;
+
+
+    // zero
+    memset( &callbacks, 0, sizeof(callbacks) );
+
+    // set callbacks
+    callbacks.set_config = &set_configuration;
+    callbacks.on_init = &on_init;
+    callbacks.on_release = &on_release;
+    callbacks.on_warn = &on_warn;
+    callbacks.on_error = &on_error;
+    callbacks.on_fatal = &on_fatal;
+    callbacks.on_ok = &on_ok;
+
+
+    // use PolySync main entry, this will give execution context to node template machine
+    return( psync_node_main_entry( &callbacks, argc, argv ) );
 }
 
 
@@ -371,17 +151,7 @@ static void on_init(
     socket = (ps_socket*) user_data;
 
     // check reference since other routines don't
-    if( socket == NULL )
-    {
-        psync_log_message(
-                LOG_LEVEL_ERROR,
-                "%s : (%u) -- invalid socket",
-                __FILE__,
-                __LINE__ );
-
-        psync_node_activate_fault( node_ref, DTC_USAGE, NODE_STATE_FATAL );
-        return;
-    }
+    ps_socket_error(socket);
 
     // init UDP socket
     ret = psync_socket_init(
@@ -390,19 +160,7 @@ static void on_init(
             SOCK_DGRAM,
             IPPROTO_UDP );
 
-    // activate fatal error and return if failed
-    if( ret != DTC_NONE )
-    {
-        psync_log_message(
-                LOG_LEVEL_ERROR,
-                "%s : (%u) -- psync_socket_init returned DTC %d",
-                __FILE__,
-                __LINE__,
-                ret );
-
-        psync_node_activate_fault( node_ref, ret, NODE_STATE_FATAL );
-        return;
-    }
+    ps_socket_init_error(ret);
 
     // set address and port
     ret = psync_socket_set_address(
@@ -410,36 +168,11 @@ static void on_init(
             UDP_ADDRESS,
             UDP_PORT );
 
-    // activate fatal error and return if failed
-    if( ret != DTC_NONE )
-    {
-        psync_log_message(
-                LOG_LEVEL_ERROR,
-                "%s : (%u) -- psync_socket_set_address returned DTC %d",
-                __FILE__,
-                __LINE__,
-                ret );
-
-        psync_node_activate_fault( node_ref, ret, NODE_STATE_FATAL );
-        return;
-    }
-
+    ps_socket_set_address_error(ret);
+	
     // set socket reuse option for multiple connections
     ret = psync_socket_set_reuse_option( socket, 1 );
-
-    // activate fatal error and return if failed
-    if( ret != DTC_NONE )
-    {
-        psync_log_message(
-                LOG_LEVEL_ERROR,
-                "%s : (%u) -- psync_socket_set_reuse_option returned DTC %d",
-                __FILE__,
-                __LINE__,
-                ret );
-
-        psync_node_activate_fault( node_ref, ret, NODE_STATE_FATAL );
-        return;
-    }
+    ps_socket_set_reuse_option_error(ret);
 
 
     my_socket = socket;
@@ -451,19 +184,8 @@ static void on_init(
             OBJECTS_MSG_NAME,
             &msg_type );
 
-    // activate fatal error and return if failed
-    if( ret != DTC_NONE )
-    {
-        psync_log_message(
-                LOG_LEVEL_ERROR,
-                "%s : (%u) -- psync_message_get_type_by_name returned DTC %d",
-                __FILE__,
-                __LINE__,
-                ret );
-
-        psync_node_activate_fault( node_ref, ret, NODE_STATE_FATAL );
-        return;
-    }
+    ps_message_get_type_by_name_error(ret);
+    
     // register subscriber for objects message
     ret = psync_message_register_listener(
             node_ref,
@@ -471,19 +193,7 @@ static void on_init(
             ps_objects_msg__handler,
             NULL );
 
-    // activate fatal error and return if failed
-    if( ret != DTC_NONE )
-    {
-        psync_log_message(
-                LOG_LEVEL_ERROR,
-                "%s : (%u) -- psync_message_register_listener returned DTC %d",
-                __FILE__,
-                __LINE__,
-                ret );
-
-        psync_node_activate_fault( node_ref, ret, NODE_STATE_FATAL );
-        return;
-    }
+    ps_message_register_listener_error(ret);
 }
 
 
@@ -493,7 +203,8 @@ static void on_release(
         const ps_diagnostic_state * const state,
         void * const user_data )
 {
-    // local vars
+    // do nothing, sleep for 10 milliseconds
+    (void) psync_sleep_micro( 10000 );
     
 }
 
@@ -537,58 +248,9 @@ static void on_ok(
         const ps_diagnostic_state * const state,
         void * const user_data )
 {
-    
-   ps_socket *socket = NULL;
-
-
-    // cast
-    socket = (ps_socket*) user_data;
-    if( socket == NULL )
-    {
-        psync_log_message(
-                LOG_LEVEL_ERROR,
-                "%s : (%u) -- invalid socket",
-                __FILE__,
-                __LINE__ );
-
-        psync_node_activate_fault( node_ref, DTC_USAGE, NODE_STATE_FATAL );
-        return;
-    }
-
 
     // sleep for 500 milliseconds
     (void) psync_sleep_micro( 100000 );
 
 
-}
-
-
-
-
-// *****************************************************
-// public definitions
-// *****************************************************
-
-//
-int main( int argc, char **argv )
-{
-    // callback data
-    ps_node_callbacks callbacks;
-
-
-    // zero
-    memset( &callbacks, 0, sizeof(callbacks) );
-
-    // set callbacks
-    callbacks.set_config = &set_configuration;
-    callbacks.on_init = &on_init;
-    callbacks.on_release = &on_release;
-    callbacks.on_warn = &on_warn;
-    callbacks.on_error = &on_error;
-    callbacks.on_fatal = &on_fatal;
-    callbacks.on_ok = &on_ok;
-
-
-    // use PolySync main entry, this will give execution context to node template machine
-    return( psync_node_main_entry( &callbacks, argc, argv ) );
 }
