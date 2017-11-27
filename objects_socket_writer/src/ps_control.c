1f138b266fe03ecc6a3cec52bd841154debd91b2
WeigangZhu <Zhu_Dean@outlook.com>
2017-11-27 15:16:09 +0800

    add some new fucntion and redefine variables

--- objects_socket_writer/src/ps_control.c
+++ objects_socket_writer/src/ps_control.c
@@ -1,16 +1,29 @@
 #include"ps_control.h"
 #include<math.h>
 
-void AEB_pid(velocity_error *vel, distance_error_t *dis, velocity_distance_error_t *vel_dis)
+
+// PID process, return control variables
+void AEB_pid(velocity_error_t *vel, distance_error_t *dis, velocity_distance_error_t *vel_dis)
 {
-	vel_dis->velocity = PID_A0 * vel->error_now + PID_A1 * vel->error_pre +PID_A2 * vel->error_pre_sec;
-	vel_dis->distance = PID_A0 * dis->error_now + PID_A1 * dis->error_pre +PID_A2 * dis->error_pre_sec;
+	double PID_A0 = PID_P*( 1 + PERIOD/PID_I + PID_D/PERIOD);
+	double PID_A1 = -PID_P*( 1 + 2 * PID_D/PERIOD);
+	double PID_A2 = -PID_P * PID_D/PERIOD;
+	
+	vel_dis->error_velocity = 
+			PID_A0 * vel->error[2] + 
+			PID_A1 * vel->error[1] +
+			PID_A2 * vel->error[0];
+		
+	vel_dis->error_distance = 
+			PID_A0 * dis->error[2] + 
+			PID_A1 * dis->error[1] +
+			PID_A2 * dis->error[0];
 }
 
-
-bool is_receive_four(int *num)
+// testing whether receive four data, in order to active PID program
+int is_receive_four(int *num)
 {
-	if (*num >0 && *num % 4 ==0 )
+	if (*num >0 && *num % 3 ==0 )
 	{
 		*num = 0;
 		return 1;
@@ -19,13 +32,31 @@ bool is_receive_four(int *num)
 		return 0;
 }
 
-bool is_object_front( double y)
+// judge whether the objects is in front
+int is_object_front( double y)
 {
 	if(abs(y) < CAR_WIDTH / 2)
 		return 1;
 	else
 		return 0;
 }
+
+// testing whether the velocity is right
+int is_velocity_right(double x)
+{
+	if( x < 0 || x >100)
+		return 0;
+	else 
+		return 1;
+}
+
+// return the relative velocity of objects with car
+double return_velocity(double x, double y)
+{
+	int temp_x = is_velocity_right(x);
+	int temp_y = is_velocity_right(y);
 	
+	return sqrt(x*x*temp_x + y*y*temp_y);
+}
 	
 
--- objects_socket_writer/src/ps_control.h
+++ objects_socket_writer/src/ps_control.h
@@ -1,17 +1,16 @@
 #ifndef PS_CONTROL_H_
 #define PS_CONTROL_H_
 
-const static double BRAKE_MAX = 60.0;  //deg
-const static double VELOCITY_EXPECT = 0.0; // M
-const static double DISTANCE_EXPECT = 0.5; // M
-const static double PID_P = 3.0;
-const static double PID_I = 40.0;
-const static double PID_D = 3.0;
-const static double PERIOD = 0.08;
-const static double PID_A0 = PID_P*( 1 + PERIOD/PID_I + PID_D/PERIOD);
-const static double PID_A1 = -PID_P*( 1 + 2 * PID_D/PERIOD);
-const static double PID_A2 = -PID_P * PID_D/PERIOD;
-const static double CAR_WIDTH = 2.0;
+static double BRAKE_MAX = 60.0;  //deg
+static double VELOCITY_EXPECT = 0.0; // M
+static double DISTANCE_EXPECT = 0.5; // M
+static double PID_P = 3.0;
+static double PID_I = 40.0;
+static double PID_D = 3.0;
+static double PERIOD = 0.08;
+static double CAR_WIDTH = 2.0;
+
+
 typedef struct velocity_distance_error
 {
 	double error_velocity;
@@ -20,35 +19,28 @@ typedef struct velocity_distance_error
 
 typedef struct velocity_error
 {
-	double error_pre_sec;
-	double error_pre;
-	double error_now;
+	double error[3];
 } velocity_error_t;
 
 typedef struct distance_error
 {
-	double error_pre_sec;
-	double error_pre;
-	double error_now;
+	double error[3];
 } distance_error_t;
 
 typedef struct velocity_distance
 {
-	double velocity;
-	double distance;
+	double velocity[4];
+	double distance[4];
 } velocity_distance_t;
 
-typedef struct raw_data
-{
-	double data_pre_thi;
-	double data_pre_sec;
-	double data_pre_fir;
-	double data_now;
-} raw_data_t;
 
-void AEB_pid(velocity_error *vel, distance_error_t *dis, velocity_distance_error_t *vel_dis);
-void assert_vel_dis();
-bool is_receive_four(int *num);
-bool is_object_front( double y);
+
+void AEB_pid(velocity_error_t *vel, distance_error_t *dis, velocity_distance_error_t *vel_dis);
+int is_receive_four(int *num);
+int is_object_front( double y);
+int is_velocity_right(double x);
+double return_velocity(double x, double y);
+
+
 #endif
 
