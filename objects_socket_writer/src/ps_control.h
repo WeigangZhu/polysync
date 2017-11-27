e291ed4e9ac802433ddeefbed8d53be1651c9263
WeigangZhu <Zhu_Dean@outlook.com>
2017-11-27 21:31:15 +0800

    just modified a function API and change some constants

--- objects_socket_writer/src/ps_control.h
+++ objects_socket_writer/src/ps_control.h
@@ -5,10 +5,10 @@ static double BRAKE_MAX = 60.0;  //deg
 static double VELOCITY_EXPECT = 0.0; // M
 static double DISTANCE_EXPECT = 0.5; // M
 static double PID_P = 3.0;
-static double PID_I = 40.0;
+static double PID_I = 0.1;
 static double PID_D = 3.0;
 static double PERIOD = 0.08;
-static double CAR_WIDTH = 2.0;
+static double CAR_WIDTH = 20.0;
 
 
 typedef struct velocity_distance_error
@@ -36,7 +36,7 @@ typedef struct velocity_distance
 
 
 void AEB_pid(velocity_error_t *vel, distance_error_t *dis, velocity_distance_error_t *vel_dis);
-int is_receive_four(int *num);
+int is_receive_four(int num);
 int is_object_front( double y);
 int is_velocity_right(double x);
 double return_velocity(double x, double y);
