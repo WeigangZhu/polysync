8fc8f206cb956b7fe5a56a32d28074be0f60eda2
WeigangZhu <Zhu_Dean@outlook.com>
2017-12-05 10:45:45 +0800

    delete many no use funcitons and definitions

--- se-writer/src/ps_control.c
+++ se-writer/src/ps_control.c
@@ -2,35 +2,6 @@
 #include<math.h>
 
 
-// PID process, return control variables
-void AEB_pid(velocity_error_t *vel, distance_error_t *dis, velocity_distance_error_t *vel_dis)
-{
-	double PID_A0 = PID_P*( 1 + PERIOD/PID_I + PID_D/PERIOD);
-	double PID_A1 = -PID_P*( 1 + 2 * PID_D/PERIOD);
-	double PID_A2 = -PID_P * PID_D/PERIOD;
-	
-	vel_dis->error_velocity = 
-			PID_A0 * vel->error[2] + 
-			PID_A1 * vel->error[1] +
-			PID_A2 * vel->error[0];
-		
-	vel_dis->error_distance = 
-			PID_A0 * dis->error[2] + 
-			PID_A1 * dis->error[1] +
-			PID_A2 * dis->error[0];
-}
-
-// testing whether receive four data, in order to active PID program
-int is_receive_four(int num)
-{
-	if (num >0 && num % 3 ==0 )
-	{
-		//*num = 0;
-		return 1;
-	}
-	else 
-		return 0;
-}
 
 // judge whether the objects is in front
 int is_object_front( double y)
@@ -59,29 +30,5 @@ double return_velocity(double x, double y)
 	return sqrt(x*x*temp_x + y*y*temp_y);
 }
 
-void ps_memory(velocity_distance_t *data, 
-			   velocity_distance_error_t *vel_dis,
-			   velocity_error_t *vel_err,
-			   distance_error_t *dis_err)
-{
-	data = (struct  velocity_distance*)malloc(sizeof(struct  velocity_distance));
-    
-    vel_dis = (struct  velocity_distance_error*)malloc(sizeof(struct  velocity_distance_error))  ;
-    
-    vel_err = (struct  velocity_error*)malloc(sizeof(struct  velocity_error));
-    
-    dis_err = (struct  distance_error*)malloc(sizeof(struct  distance_error));
-}
-
-void ps_free_memory(velocity_distance_t *data, 
-			   velocity_distance_error_t *vel_dis,
-			   velocity_error_t *vel_err,
-			   distance_error_t *dis_err)
-{
-	free(data);
-	free(vel_dis);
-	free(vel_err);
-	free(dis_err);
-}
 	
 
--- se-writer/src/ps_control.h
+++ se-writer/src/ps_control.h
@@ -1,50 +1,9 @@
 #ifndef PS_CONTROL_H_
 #define PS_CONTROL_H_
-
-static double BRAKE_MAX = 60.0;  //deg
-static double VELOCITY_EXPECT = 0.0; // M
-static double DISTANCE_EXPECT = 0.5; // M
-static double PID_P = 3.0;
-static double PID_I = 0.1;
-static double PID_D = 3.0;
-static double PERIOD = 0.08;
-static double CAR_WIDTH = 20.0;
-
-
-typedef struct velocity_distance_error
-{
-	double error_velocity;
-	double error_distance;
-} velocity_distance_error_t;
-
-typedef struct velocity_error
-{
-	double error[3];
-} velocity_error_t;
-
-typedef struct distance_error
-{
-	double error[3];
-} distance_error_t;
-
-typedef struct velocity_distance
-{
-	double velocity[4];
-	double distance[4];
-} velocity_distance_t;
-
-
-
-void AEB_pid(velocity_error_t *vel, distance_error_t *dis, velocity_distance_error_t *vel_dis);
-int is_receive_four(int num);
+static double CAR_WIDTH = 3.0;
 int is_object_front( double y);
 int is_velocity_right(double x);
 double return_velocity(double x, double y);
-
-void ps_memory(velocity_distance_t *, 
-			   velocity_distance_error_t *,
-			   velocity_error_t *,
-			   distance_error_t *);
 
 #endif
 
