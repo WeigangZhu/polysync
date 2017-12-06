e04cc54434e26411f2ff99697fc6bc1c7cde0da1
WeigangZhu <Zhu_Dean@outlook.com>
2017-12-06 09:42:01 +0800

    delete ps_control.c ps_control.h file, because of it is useless

--- se-writer/src/ps_control.h
+++
@@ -1,9 +0,0 @@
-#ifndef PS_CONTROL_H_
-#define PS_CONTROL_H_
-static double CAR_WIDTH = 3.0;
-int is_object_front( double y);
-int is_velocity_right(double x);
-double return_velocity(double x, double y);
-
-#endif
-
