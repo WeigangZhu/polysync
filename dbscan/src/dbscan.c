6ee7896074b3e6aced53f8a43f7c6f5da14bb566
WeigangZhu <Zhu_Dean@outlook.com>
2017-12-31 10:42:52 +0800

    the source file of the DBSCAN

---
+++ dbscan/src/dbscan.c
@@ -0,0 +1,29 @@
+#include"dbscan.h"
+
+int main(int argc, char* argv[])  
+    {  
+        if( argc != 5 )  
+        {  
+            printf("this program need 5 parematers to run,"  
+                    "\n\t\tthe first to indicate the neighborhood"  
+                    "\n\t\tthe second to indicate the MinPts"  
+                    "\n\t\tthe third to indicate the filename contain data"  
+                    "\n\t\tthe fourth to indicate the data size\n");  
+            exit(0);  
+        }  
+        srand((unsigned)time(NULL));  
+        neighborhood = atof(argv[1]);  
+        MinPts = atoi(argv[2]);  
+        strcat(filename, argv[3]);  
+        data_size = atoi(argv[4]);  
+      
+        Init();  
+        ReadData();  
+        calculateDistance_BetweenAll();  
+        statisticCoreObject();  
+        //showInformation();  
+        setCoreObject();  
+        //testQueue();  
+        DBSCAN();  
+        return 0;  
+    }  
